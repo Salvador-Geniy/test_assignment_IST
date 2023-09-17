e83a393d933777913cdd9c71f98dd4dff707b595
Alexey <alexderendyaev@ya.ru>
2023-09-17 16:48:02 -0300

    6. TestListView

--- service/lab_app/views.py
+++ service/lab_app/views.py
@@ -1,11 +1,19 @@
 
 from django.db.models import Prefetch
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
-from .models import Test
+from .models import Test, Scores
 from .serializers import TestSerializer
 
 
 class TestListView(ListAPIView):
-    queryset = Test.objects.filter(status__is_active=True).prefetch_related(Prefetch('scores')).order_by('laboratory')
+    permission_classes = [IsAuthenticated]
+    queryset = Test.objects.filter(status__is_active=True).prefetch_related(
+        Prefetch('scores', queryset=Scores.objects.all().select_related(
+            'indicator_metric',
+            'indicator_metric__metric',
+            'indicator_metric__indicator',
+            'indicator_metric__references'))
+    )
     serializer_class = TestSerializer
